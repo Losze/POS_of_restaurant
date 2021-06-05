459d8b6aa3bba280286115b7aa130bb239cc6551
Vũ Đức Mạnh <manh.vu1407@hcmut.edu.vn>
2021-06-05 21:41:11 +0700

    add homepage

--- src/app/controllers/SiteCotroller.js
+++ src/app/controllers/SiteCotroller.js
@@ -2,11 +2,11 @@ const Food = require('../models/Food')
 
 class SiteController {
     index(req, res){
-        // res.render('home')
-        Food.find({}, function(err, foods) {
-            if (!err) res.json(foods)
-            else res.status(400).json({error: 'Error!!!'})
-        })
+        res.render('home')
+        // Food.find({}, function(err, foods) {
+        //     if (!err) res.json(foods)
+        //     else res.status(400).json({error: 'Error!!!'})
+        // })
     }
     show(req, res){
         res.send('home')
