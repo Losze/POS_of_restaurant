c82b14b8cd5050a2322efdba12de353f83e0808a
Losze <long.nt1608@gmail.com>
2021-06-19 22:36:23 +0700

    update 1 food

---
+++ src/app/controllers/OrderCotroller.js
@@ -0,0 +1,83 @@
+const Food = require('../models/Food')
+const {multi, mongooseToObject} = require('../../util/mongoose')
+const jwt = require('jsonwebtoken')
+const User = require('../models/User')
+class OrderController {
+
+    //GET method
+    index(req, res, next){
+        var token = jwt.decode(req.cookies.token)
+        console.log(token)
+        if(token == null){
+            res.redirect("/");
+        }
+        else{
+            var phone = token.phone
+            User.findOne({ phone: phone}).
+            then(data => {                
+                if (data) {
+                    console.log(data);
+                    if (data.role != 'admin'){
+                        res.redirect("/");
+                    }
+                    else {
+                        Food.find({})
+                        .then(food => res.render('order', {
+                            food: multi(food)
+                        })) 
+                        .catch(next)
+                    }
+                }
+
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json('Server Error');
+            });
+
+        }
+    }
+    // show(req, res){
+    //     res.send('news detail')
+    // }
+    create(req, res){
+        res.render('create')
+    }
+
+    update(req, res, next){
+        Food.findById(req.params.id)
+            .then(food => res.render('update', {
+                food: mongooseToObject(food)
+            }))
+            .catch(next)
+    }
+
+    delete(req, res, next){
+        Food.deleteOne({_id: req.params.id})
+            .then(() => res.redirect('/order'))
+            .catch(error => {
+
+            })
+    }
+
+    edit(req, res, next){
+        Food.updateOne({_id: req.params.id}, req.body)
+            .then(() => res.redirect('/order'))
+            .catch(error => {
+
+            })
+    }
+
+    store(req, res){
+        //res.json(req.body)
+        const formData = req.body
+        const newFood = new Food(formData)
+        newFood.save()
+            .then(() => res.redirect('/order'))
+            .catch(error => {
+
+            })
+    }
+}
+
+module.exports = new OrderController
---
+++ src/app/controllers/SiteCotroller.js
@@ -0,0 +1,97 @@
+const Food = require('../models/Food')
+const cartModel = require('../models/Cart');
+const jwt = require('jsonwebtoken');
+const orderModel = require('../models/Order');
+const { multipleMongooseToObject } = require('../../resource/util/mongoose');
+const { updateOne } = require('../models/Food');
+const Usr = require('../models/User')
+
+
+class SiteController {
+    home(req, res, next) {
+        Food.find({}).sort({_id: -1}).limit(4)
+            .then(foods => {
+                res.render('home', {
+                    foods: multipleMongooseToObject(foods)
+                })
+            })
+            .catch(next)
+    }
+    showabout(req, res) {
+        res.render('about')
+    }
+    showcontact(req, res) {
+        res.render('contact')
+    }
+
+    showmenu(req, res, next) {
+        Food.find({})
+            .then(foods => {
+                res.render('menu', {
+                    foods: multipleMongooseToObject(foods)
+                })
+            })
+            .catch(next)
+    }
+
+    history(req, res, next) {
+        var token = req.cookies.token;
+        var User = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        orderModel.find({phone: User.phone})
+            .then(order => {
+                    res.render('history', {
+                        order: multipleMongooseToObject(order),
+                       
+                    })
+                })
+    }
+
+    
+
+    addToCart(req, res, next) {
+        var token = req.cookies.token;
+        var User = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        var data = req.body;
+        var codeFood = data.code;
+        var num = data.number;
+        
+        cartModel.findOne({ phone: User.phone, })
+            .then(cart => {
+                console.log("start");
+                var consCart = cart.cart;
+                console.log(consCart);
+                let itemCode = Object.keys(cart.cart);
+                var exist = false;
+                for (let i = 0; i < itemCode.length; i++) {
+                    if (itemCode[i] == codeFood) {
+                        consCart[codeFood].number = num;
+                        exist = true;
+                    }
+                }
+                if (!exist) {
+                    consCart[codeFood] = {
+                        "number": num,
+                    }
+                }
+                return consCart;
+            })
+            .then(consCart => {
+                cartModel.deleteOne({ phone: User.phone })
+                    .then(() => {
+                        var newCart = {};
+                        newCart.phone = User.phone;
+                        newCart.cart = consCart;
+                        const SaveCart = new cartModel(newCart);
+                        SaveCart.save();
+
+                    });
+            })
+            .catch(err => {
+                console.log(err);
+            })
+
+
+    }
+}
+
+module.exports = new SiteController;
