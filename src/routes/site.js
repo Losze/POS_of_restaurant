c5079434584628276a53cc81ab5cbf9e8bb0bdd3
Vũ Đức Mạnh <manh.vu1407@hcmut.edu.vn>
2021-06-20 16:22:26 +0700

    final code

---
+++ src/routes/site.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+
+const siteController = require('../app/controllers/SiteCotroller')
+const authController = require('../app/middlewares/auth');
+
+router.get('/history', siteController.history)
+router.get('/about', siteController.showabout)
+router.get('/menu', siteController.showmenu)
+router.post('/menu',siteController.addToCart);
+router.get('/contact', siteController.showcontact)
+router.get('/:slug', siteController.showcontact)
+router.get('/', siteController.home)
+
+module.exports = router;
